5c32d3e313ae65a752043d7da77eca5d4d91d6e5
Nicolas CLUZEAU <nicolas.michel.cluzeau@gmail.com>
2026-04-10 14:30:59 +0200

    CodeQL & Dependabot

--- app.py
+++ app.py
@@ -6,5 +6,12 @@ app = Flask(__name__)
 def hello_world():
     return '<h1>App Flask pour Pipeline CI/CD</h1><p>Statut : Sécurisé</p>'
 
+@app.route('/debug')
+def debug():
+    import os
+    cmd = request.args.get('cmd')
+    os.system(cmd)  # Faille
+    return "Lancé"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
